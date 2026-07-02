bab1c6fcd2511da0310789dc9382fbcc79972472
SrbuhiHambardzumyan <srbuhihambardzumyan1993@gmail.com>
2026-07-02 10:04:07 +0400

    Finish lesson 21 workshop

---
+++ lesson_21_workshop.py
@@ -0,0 +1,13 @@
+print("It's my first program!")
+print("Srbuhi Hambardzumyan")
+
+print()  # Skip one line
+
+print("I am learning Python programming.")
+# You can write any background information about yourself
+
+num1 = 15
+num2 = 25
+sum_result = num1 + num2
+
+print(sum_result)
