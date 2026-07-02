8e196bb976559d2a7842afec586e6055b4d03c40
sophiesardaryan <sofi.sardaryan.9191@gmail.com>
2026-07-02 10:02:49 +0400

    added a new file in Sophie_Sardaryan branch

---
+++ lesson_21_workshop.py
@@ -0,0 +1,8 @@
+print ("It's my first program")
+print ("Sophie Sardaryan")
+print("\n")
+print("I study in the Software Engineering department.")
+a = 5
+b = 6
+sum = a + b # I defined a new variable that stores the sum of the two variables.
+print(sum)
