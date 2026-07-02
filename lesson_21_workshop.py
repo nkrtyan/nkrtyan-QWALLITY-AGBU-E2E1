72b0849c80542b176981b71ee23121f749b9d5ea
Hasmik Hakobyan <hashakobyan1988@gmail.com>
2026-07-02 10:54:27 +0400

    Add initial version of lesson_21_workshop.py

---
+++ lesson_21_workshop.py
@@ -0,0 +1,9 @@
+print("it's my first program!")
+print("Hasmik Hakobyan", end="\n\n")
+print("I studed the pedagogical university of Armenia")
+x=5
+y=7
+z=x+y
+print(z)
+print("my first file in python")
+# My first python file
