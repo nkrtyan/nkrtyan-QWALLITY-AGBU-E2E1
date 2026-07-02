000a1118b0f49938ffbfe43b3484ba60929a7736
lilsuqiasyan <lil.suqiasyan@gmail.com>
2026-07-02 11:26:22 +0400

    Lilit Suqiasyan

--- README.md
+++
@@ -1,3 +0,0 @@
-# nkrtyan-QWALLITY-AGBU-E2E1
-Educational purpose
-Lilit Suqiasyan_Homework_VScode
---
+++ lesson_21_workshop.py
@@ -0,0 +1,6 @@
+print("Its my first program")
+print("Lilit Suqiasyan") 
+print("I graduated from Yerevan State University Faculty of Geography and Giolagy\n")
+x=10 
+y=16
+print(x+y)#I love my QA Instructor and QA lessons
