3250fe51c9550e01997ed0b0504ceefe72d8e09b
Lilit Suqiasyan <lil.suqiasyan@gmail.com>
2026-07-02 11:28:14 +0400

    Delete lesson_21_workshop.py

--- lesson_21_workshop.py
+++
@@ -1,7 +0,0 @@
-print("Its my first program")
-print("Lilit Suqiasyan") 
-
-print("I graduated from Yerevan State University Faculty of Geography and Giolagy. I can play on the piano")
-x=10 #How can I print variables in one line
-y=15
-print(x+y)#I didnt listen carufally and didnt understand how can I skip the line
