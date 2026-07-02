dfd3844890ccb0205bc6c4dc53ca71c3da2a48d8
Ofelya Shahbazyan <shahbazyanofelya.9@gmail.com>
2026-07-02 10:25:38 +0400

    Add lesson 21 workshop solution

---
+++ lesson_21_workshop.py
@@ -0,0 +1,10 @@
+print("It's my first program!")
+print("Ofelya Shahbazyan")
+print()
+print("I'm a QA Engineer with Java Development background")
+
+# Calculate sum of two numbers and print
+number_1 = 9
+number_2 = 2
+sum = number_1 + number_2
+print("Sum of number_1 and number_2 is: ", sum)
